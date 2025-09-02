4ea2fddb457cd08b546516abc745e7bbfe7eb02a
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2025-09-02 22:07:24 +0600

    fix: email smtp port change and ssl check

--- config/settings.py
+++ config/settings.py
@@ -213,8 +213,9 @@ CELERY_BEAT_SCHEDULE = {
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv("MAIL_USER")
 EMAIL_HOST_PASSWORD = os.getenv("MAIL_PASSWORD")
 
