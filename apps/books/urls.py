08c0dfcd8f17acbcadf20b31196549842c3e9109
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2025-09-04 15:52:18 +0600

    fix: url pattern fix

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -4,9 +4,9 @@ from rest_framework_nested import routers
 from .views import BookViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
-router.register(r"books", BookViewSet, basename="book")
+router.register(r"", BookViewSet, basename="book")
 
-reviews_router = routers.NestedDefaultRouter(router, r"books", lookup="book")
+reviews_router = routers.NestedDefaultRouter(router, r"", lookup="book")
 reviews_router.register(r"reviews", ReviewViewSet, basename="book-reviews")
 
 urlpatterns = [
